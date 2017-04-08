2a549f9eac086b3a20fbc91598408a11e9ab4607
owenc <owenc@DESKTOP-IMJD3ST>
2017-04-08 12:51:59 +0100

    Add project files.

---
+++ wpd2coursework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wpd2coursework.MvcApplication" Language="C#" %>
---
+++ wpd2coursework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wpd2coursework.Startup))]
+namespace wpd2coursework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
