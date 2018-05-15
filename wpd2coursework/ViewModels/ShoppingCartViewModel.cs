967c097f3d80344ed2aa04f9936bb778ede318bc
Kezanub <33518859+Kezanub@users.noreply.github.com>
2018-05-15 23:01:24 +0100

    Add files via upload

---
+++ wpd2coursework/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using wpd2coursework.Models;
+
+namespace wpd2coursework.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
